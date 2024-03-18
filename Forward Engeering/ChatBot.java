a74b7029f7749ecd40da63f71c3b6895e91a5b64
chinezprost <playschan@gmail.com>
2024-03-18 09:14:08 +0200

    modified argo uml diagram and generated .java from argo uml class diagram (forward engineering)

---
+++ Forward Engeering/ChatBot.java
@@ -0,0 +1,26 @@
+import java.util.Vector;
+
+public class ChatBot {
+
+  public String nume;
+
+  public String versiune;
+
+  public Integer newAttr;
+
+    public Vector  myUtilizator;
+    public Vector  mySistemNotificari;
+    public Vector  myAdministrareEveniment;
+
+  public String interpreteazaMesajul(String mesaj) {
+  return null;
+  }
+
+  public String solicitaCalificari() {
+  return null;
+  }
+
+  public void trimiteNotificari() {
+  }
+
+}
---
+++ Forward Engeering/Eveniment.java
@@ -0,0 +1,27 @@
+import java.util.Vector;
+
+public class Eveniment {
+
+  public int idEveniment;
+
+  public String nume;
+
+  public String locatie;
+
+  public String descriere;
+
+  public String tipEveniment;
+
+  public String data;
+
+    public Vector  myAdministrareEveniment;
+
+  public String obtineDetalii() {
+  return null;
+  }
+
+  public String obtineRecomandari(String bazaPreferinte) {
+  return null;
+  }
+
+}
